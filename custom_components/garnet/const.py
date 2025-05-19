c9aec37f5c7e752b268073cdd5de553df93c4bcb
danTapps <daniel.terryn@gmail.com>
2025-05-19 12:51:03 -0400

    Added definitions for BTP7

--- custom_components/garnet/const.py
+++ custom_components/garnet/const.py
@@ -8,13 +8,20 @@ DOMAIN = "garnet"
 
 PLATFORMS = [Platform.SENSOR]
 
+MFR_ID_BTP3 = 305
+MFR_ID_BTP7 = 3264
+
 
 class GarnetTypes(StrEnum):
     """Garnet value types."""
 
     FRESH_TANK = "fresh_tank"
+    FRESH_TANK2 = "fresh_tank2"
     BLACK_TANK = "black_tank"
+    BLACK_TANK2 = "black_tank2"
     GREY_TANK = "grey_tank"
+    GREY_TANK2 = "grey_tank2"
+    GREY_TANK3 = "grey_tank3"
     LPG_TANK = "lpg_tank"
     LPG_2_TANK = "lpg_2_tank"
     GALLEY_TANK = "galley_tank"
